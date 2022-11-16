1c3a3aa698064da827a893e526d4e7bfecc1f3e1
MackieJG <joshuamackie10@googlemail.com>
2022-11-16 19:14:49 +0000

    Final Solutions

--- pub_lab/run_tests.py
+++ pub_lab/run_tests.py
@@ -1,4 +1,7 @@
 import unittest
-from tests.pub_tests import TestPub
+from tests.pub_test import TestPub
+from tests.drinks_test import TestDrink
+from tests.customer_test import TestCustomer
+from tests.food_test import TestFood
 if __name__ == '__main__':
     unittest.main()
